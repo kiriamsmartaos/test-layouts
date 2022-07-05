6f218f59636bdbe6518f283cf8594e7b9132e70f
Kiriam Campobadal <kiriam@smartaos.com>
2022-07-05 13:43:55 +0200

    first two layouts

---
+++ webpack.mix.js
@@ -0,0 +1,3 @@
+const mix = require('laravel-mix');
+mix.sass('resources/sass/app1.scss', 'public/css').options({processCssUrls: false});
+mix.sass('resources/sass/app2.scss', 'public/css').options({processCssUrls: false});
