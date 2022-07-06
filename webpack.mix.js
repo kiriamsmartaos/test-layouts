be6b54ceb3586ba6f131162e90d6cc7f4bc065cd
Kiriam Campobadal <kiriam@smartaos.com>
2022-07-06 16:52:36 +0200

    Layout2 changed with new implementations

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,2 @@
 const mix = require('laravel-mix');
-mix.sass('resources/sass/app1.scss', 'public/css').options({processCssUrls: false});
-mix.sass('resources/sass/app2.scss', 'public/css').options({processCssUrls: false});
-mix.sass('resources/sass/app3.scss', 'public/css').options({processCssUrls: false});
+mix.sass('resources/sass/app1.scss', 'public/css').options({processCssUrls: false}).sass('resources/sass/app2.scss', 'public/css').options({processCssUrls: false}).sass('resources/sass/app3.scss', 'public/css').options({processCssUrls: false});
