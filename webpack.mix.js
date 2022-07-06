f6a0c20aac604189167dc21e5bcdfc4af5517e05
Kiriam Campobadal <kiriam@smartaos.com>
2022-07-06 10:25:25 +0200

    Layout3 and git errors fixed

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,3 +1,4 @@
 const mix = require('laravel-mix');
 mix.sass('resources/sass/app1.scss', 'public/css').options({processCssUrls: false});
 mix.sass('resources/sass/app2.scss', 'public/css').options({processCssUrls: false});
+mix.sass('resources/sass/app3.scss', 'public/css').options({processCssUrls: false});
